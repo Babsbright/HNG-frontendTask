db5e86295c07ae8595e9ba95915ee474e880f8bf
Tolulope Babatunde <babsbright69@gmail.com>
2022-10-24 15:37:34 +0100

    Worked on app.js

--- firsttask/src/App.js
+++ firsttask/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+                
+import img1 from './Image/taskimg (5).png'
+import img2 from './Image/taskimg (3).png'
+import img3 from './Image/taskimg (4).png'
+import slack from './Image/slack.png'
+import github from './Image/Icon.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='profile'>
+        <img className='profile__img' src={img1}  />
+        <h2 className='twitter'>Annette Black</h2>
+        <h2 className='slack'>slack</h2>
+      </div>
+
+      <div className='btns'>
+        <button className='btn__zuri'><a href='https://training.zuri.team/'>Twitter Link</a></button>
+        <button className='books'><a href='https://books.zuri.team/'>Zuri Team</a></button>
+        <button className='books'><a href='http://books.zuri.team'>Zuri Books</a></button>
+        <button className='book__python'><a href='https://books.zuri.team/'>Python Books</a></button>
+        <button className='pitch'><a href='https://background.zuri.team'>Background check for Coders</a></button>
+        <button className='book__design'><a href='https://books.zuri.team/design-rules'>Design Books</a></button>
+
+      </div>
+      <div className='icons'>
+        <img src = {slack} />
+        <img src = {github} />
+       
+      </div>
+   
+   
+      <footer>
+        <img src = {img2} />
+        <p>HNG Internship 9 Frontend Task</p>
+        <img src ={img3} />
+      </footer>
     </div>
   );
 }
