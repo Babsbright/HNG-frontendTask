00e0660230c11a673b7214bcd3f44f1b0e4223c5
Tolulope Babatunde <babsbright69@gmail.com>
2022-10-27 11:59:20 +0100

    Added final touches

--- firsttask/src/App.js
+++ firsttask/src/App.js
@@ -8,34 +8,47 @@ import img3 from './Image/taskimg (4).png'
 import slack from './Image/slack.png'
 import github from './Image/Icon.png'
 import vector from './Image/taskimg (2).png'
+import vector2 from './Image/desktop_share.png'
 
 function App() {
   return (
     <div className="App">
-      <img className='vector' src={vector} />
+
+      {/* PROFILE */}
+      
+      <img className='vector' src={vector} alt='mobile menu' />
+      <img className='vector2' src={vector2} alt='desktop share' />
 
       <div className='profile'>
 
-        <img className='profile__img' src={img1} alt='profile' />
-        <h2 className='twitter'>Annette Black</h2>
-        <h2 className='slack'>slack</h2>
+        <img id='profile__img' src={img1} alt='profile' />
+        <h2 id='twitter'>Annette Black</h2>
+        <h2 id='slack' className='slack'>Tolulope Bright</h2>
       </div>
+
+      {/* BUTTONS */}
 
       <div className='btns'>
-        <button className='btn__zuri'><a href='https://training.zuri.team/'>Twitter Link</a></button>
-        <button className='books'><a href='https://books.zuri.team/'>Zuri Team</a></button>
-        <button className='books'><a href='http://books.zuri.team'>Zuri Books</a></button>
-        <button className='book__python'><a href='https://books.zuri.team/'>Python Books</a></button>
-        <button className='pitch'><a href='https://background.zuri.team'>Background check for Coders</a></button>
-        <button className='book__design'><a href='https://books.zuri.team/design-rules'>Design Books</a></button>
+        <button id='btn__zuri'><a href='https://training.zuri.team/'>Twitter Link</a></button>
+        <button id='books'><a href='https://books.zuri.team/'>Zuri Team</a></button>
+        <button id='books'><a href='http://books.zuri.team'>Zuri Books</a></button>
+        <button id='book__python'><a href='https://books.zuri.team/python-for-beginners?ref_id=<tolulopeBright>'>Python Books</a></button>
+        <button id='pitch'><a href='https://background.zuri.team'>Background check for Coders</a></button>
+        <button id='book__design'><a href='https://books.zuri.team/design-rules'>Design Books</a></button>
 
       </div>
+
+
+      {/* ICONS */}
+
       <div className='icons'>
         <img src={slack} alt='slack logo' />
         <img src={github} alt='github logo' />
 
       </div>
 
+
+      {/* FOOTER */}
 
       <footer>
         <hr />
