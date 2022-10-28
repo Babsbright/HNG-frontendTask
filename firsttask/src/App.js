359bb105fc596eb2ab0becfda77cbf7be3bd7e58
Tolulope Babatunde <babsbright69@gmail.com>
2022-10-28 16:20:05 +0100

    changed profile picture and name

--- firsttask/src/App.js
+++ firsttask/src/App.js
@@ -9,6 +9,7 @@ import slack from './Image/slack.png'
 import github from './Image/Icon.png'
 import vector from './Image/taskimg (2).png'
 import vector2 from './Image/desktop_share.png'
+import profile from './Image/tolu.png'
 
 function App() {
   return (
@@ -21,8 +22,9 @@ function App() {
 
       <div className='profile'>
 
+        <img id='profile__img' src={profile} alt='profile' />
         <img id='profile__img' src={img1} alt='profile' />
-        <h2 id='twitter'>Annette Black</h2>
+        <h2 id='twitter'>Tolulope Bright</h2>
         <h2 id='slack' className='slack'>Tolulope Bright</h2>
       </div>
 
